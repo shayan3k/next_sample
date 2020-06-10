165323d5aa8d624b41ff6189cd08f1f8098c4bbd
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-06-10 11:00:00 +0430

    clear inside student component.location of file is on route/dashbord/index js

--- src/student/routes/Dashboard/index.js
+++ src/student/routes/Dashboard/index.js
@@ -1,5 +1,6 @@
-import React, { Fragment } from 'react'
+import React, { useState,Fragment,useEffect  } from 'react';
 import './Dashboard.scss'
+import axios from 'axios';
 
 import { LineShadow } from "../../components/Charts";
 import { conversionChartConfig } from "../../constants/chartConfig";
@@ -17,87 +18,20 @@ import Book4 from '../../assets/images/books/book04.jpg'
 import Book5 from '../../assets/images/books/book05.jpg'
 import Book6 from '../../assets/images/books/book06.jpg'
 
-export default function Dashboard() {
+
+
+
+export default function Dashboard(props) {
+   
+  
+   
+   
     return (
         <Fragment>
             <Header />
             <Menu />
             <main>
-                <section className="dashboard-wrapper">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-12">
-                                <BreadCrumb routeTitle="میز مطالعه" />
-                            </div>
-
-                            <div className="col-md-6">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="card-title">
-                                            <span>کلاس های امروز من</span>
-                                        </div>
-                                        <div className="today-classes">
-                                            <TodayClass
-                                                image={Book1}
-                                                title="ادبیات فارسی"
-                                                description="کلاس آنلاین نکته و ادبیات فارسی نظام جدید استاد محمد واعظی"
-                                                date="دوشنبه 12 خرداد 1399 ,ساعت 13:00"
-                                                url="/"
-                                            />
-                                            <TodayClass
-                                                image={Book2}
-                                                title="زبان فارسی"
-                                                description="کلاس آنلاین نکته و زبان فارسی نظام جدید استاد محمد واعظی"
-                                                date="دوشنبه 12 خرداد 1399 ,ساعت 14:00"
-                                                url="/"
-                                            />
-                                            <TodayClass
-                                                image={Book3}
-                                                title="دین و زندگی"
-                                                description="کلاس آنلاین نکته و دین و زندگی نظام جدید استاد محمد واعظی"
-                                                date="دوشنبه 12 خرداد 1399 ,ساعت 15:00"
-                                                url="/"
-                                            />
-                                            <TodayClass
-                                                image={Book4}
-                                                title="زبان انگلیسی"
-                                                description="کلاس آنلاین نکته و زبان انگلیسی نظام جدید استاد محمد واعظی"
-                                                date="دوشنبه 12 خرداد 1399 ,ساعت 16:00"
-                                                url="/"
-                                            />
-                                            <TodayClass
-                                                image={Book5}
-                                                title="شیمی"
-                                                description="کلاس آنلاین نکته و شیمی نظام جدید استاد محمد واعظی"
-                                                date="دوشنبه 12 خرداد 1399 ,ساعت 17:00"
-                                                url="/"
-                                            />
-                                            <TodayClass
-                                                image={Book6}
-                                                title="ریاضیات"
-                                                description="کلاس آنلاین نکته و ریاضیات نظام جدید استاد محمد واعظی"
-                                                date="دوشنبه 12 خرداد 1399 ,ساعت 10:00"
-                                                url="/"
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="card-title">
-                                            <span>نمودار پیشرفت شما</span>
-                                        </div>
-                                        <div className="dashboard-line-chart">
-                                            <LineShadow {...conversionChartConfig} />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+              {props.children}
             </main>
             <Footer />
         </Fragment>
